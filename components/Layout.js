96600e4ee4e7631689bbbc62da7234c909d646d6
sid1608 <akarsiddharth@gmail.com>
2022-04-10 15:00:12 +0530

    added header link

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import React from 'react'
-import {AppBar,Toolbar,Typography,Container} from "@mui/material"
+import NextLink from 'next/link';
+import {AppBar,Toolbar,Typography,Container, Link} from "@mui/material"
 import useStyles from "../utils/styles";
 const Layout = ({children}) => {
     const classes = useStyles()
@@ -11,7 +12,24 @@ const Layout = ({children}) => {
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-            <Typography>amazona</Typography>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>amazona</Typography>
+              </Link>
+                
+            </NextLink>
+            <div className={classes.grow}> 
+
+            </div>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+            
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>  
